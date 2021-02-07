ac51a2c2bc0eb16acd1bfce7b5916a7a0a11ed48
Pavan Aditya M S <pavanaditya.ms@stupify.in>
2021-02-07 18:18:16 +0530

    COMMENTS ADDED

--- prototype-pattern.js
+++ prototype-pattern.js
@@ -9,8 +9,15 @@ Human.prototype.gender = 'female';
 let gent = new Human();
 let lady = new Human();
 
+// ? Adding a property will add/override it to clone
+gent.gender1 = 'male';
+lady.gender1 = 'female';
+
+// ? Adding a prototype will add/override it to original
 gent.__proto__.gender = 'male';
+lady.__proto__.gender = 'female';
 
 console.log(lady.gender);
-// console.log(lady.gender);
-// console.log(new Human());
+console.log(gent.gender);
+console.log(lady.gender1);
+console.log(gent.gender1);
--- singleton-pattern.js
+++ singleton-pattern.js
@@ -1,27 +1,29 @@
 let Singleton = function () {
-    let instance;
- 
-    function createInstance() {
+    let instance; // ? initialization
+
+    function createInstance () {
         let object = new Object("I am the instance");
         return object;
     }
 
     return {
         getInstance: function () {
-            if (!instance) {
-                instance = createInstance();
+            if (!instance) { // ! same instance will be created if the if-cond is present
+            instance = createInstance();
             }
             return instance;
         }
     };
 }();
 
-function run() {
- 
+function run () {
+
     let instance1 = Singleton.getInstance();
     let instance2 = Singleton.getInstance();
     console.log(instance1);
-    console.log((instance1 === instance2)?"Same instance is again called, so the value of the second is not changed. Singleton can be instantiated only once ":" New Instance is created");  
+    console.log((instance1 === instance2) ?
+        "Same instance is again called, so the value of the second is not changed. Singleton can be instantiated only once "
+        : " New Instance is created");
     console.log(instance2);
 }
 
